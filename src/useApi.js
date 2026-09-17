b62346a83a45772c8997c4ff18962c7b0f550dc6
Kenedy-GrooveDev <kenedylyimo211@gmail.com>
2026-09-17 22:15:21 +0300

    #skip styled the message body

--- src/useApi.js
+++ src/useApi.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import kenedy from 'kenedy'
 
 const useApi = (url, mapResults = (result) => result) => {
   const [data, setData] = useState()
