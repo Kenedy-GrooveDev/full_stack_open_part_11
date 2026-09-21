cf9761ad66c3072858f9ea8e91119e3fabc28a78
Kenedy-GrooveDev <kenedylyimo211@gmail.com>
2026-09-21 13:58:07 +0300

    testing failure in an updated workflow

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import LoadingSpinner from './LoadingSpinner'
 import ErrorMessage from './ErrorMessage'
 import PokemonPage from './PokemonPage'
 import PokemonList from './PokemonList'
+import Kenedy form 'Kenedy'
 
 const mapResults = ({ results }) =>
   results.map(({ url, name }) => ({
